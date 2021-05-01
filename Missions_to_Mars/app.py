bb1d51052a5de521c78dec5c5c4e7f2ab0d58aab
unknown <nguye453@cougars.csusm.edu>
2021-05-01 09:40:10 -0700

    changing location of html

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -6,8 +6,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-app.config['MONGO_URI'] = "mongodb://localhost:27017/mars_info"
-mongo = PyMongo(app)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_info")
 
 @app.route("/")
 def home():
